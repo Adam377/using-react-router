b5c1e0906e724ea3b38a669b5d17c3bc39800bc4
adamhaigh <adam.haigh@takepayments.com>
2022-11-21 13:45:18 +0000

    added router to project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Route } from 'react-router-dom';
+
+import Products from './components/pages/Products';
+import Users from './components/Users';
+import Welcome from './components/pages/Welcome';
+
+function App() {
+  return (
+    <div>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+    </div>
+  );
+}
+
+export default App;
